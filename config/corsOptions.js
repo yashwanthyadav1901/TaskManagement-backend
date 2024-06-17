5c4114a7d8e8156032e3ed584812237c108b52d5
yashwanth <yashwanthyadav481@gmail.com>
2024-06-17 17:14:50 +0530

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,6 +2,7 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
+        console.log('Origin received:', origin);
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
